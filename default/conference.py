dd9f1293920e2fbd2f10f97f79ea481a6d315ed3
pox <pox@gmail.com>
2016-12-06 06:05:18 -0500

    Added an endpoint API method to conference.py

--- default/conference.py
+++ default/conference.py
@@ -235,13 +235,35 @@ class ConferenceApi(remote.Service):
         """Return user profile."""
         return self._doProfile()
 
-
     @endpoints.method(ProfileMiniForm, ProfileForm,
             path='profile', http_method='POST', name='saveProfile')
     def saveProfile(self, request):
         """Update & return user profile."""
         return self._doProfile(request)
 
+    @endpoints.method(message_types.VoidMessage, ConferenceForms,
+            path='filterPlayground',
+            http_method='GET', name='filterPlayground')
+    def filterPlayground(self, request):
+        q = Conference.query()
+        # simple filter usage:
+        q = q.filter(Conference.city == "Paris")
+
+        # advanced filter building and usage
+        # field = "city"
+        # operator = "="
+        # value = "London"
+        # f = ndb.query.FilterNode(field, operator, value)
+        # q = q.filter(f)
+
+        # TODO
+        # add 2 filters:
+        # 1: city equals to Chicago
+        # 2: topic equals "Medical Innovations"
+
+        return ConferenceForms(
+            items=[self._copyConferenceToForm(conf, "") for conf in q]
+        )
 
 # registers API
 api = endpoints.api_server([ConferenceApi])
